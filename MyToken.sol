dd085364550da5cc3ae5279f88b18cd507ec1503
lunar-huang <43958511+lunar-huang@users.noreply.github.com>
2025-02-12 17:15:54 +0800

    add mint function

--- MyToken.sol
+++ MyToken.sol
@@ -8,4 +8,8 @@ contract MyToken is ERC20, Ownable {
     constructor() ERC20("MyToken", "MTK") Ownable(msg.sender) {
         _mint(msg.sender, 1000000 * 10**18); // 初始供应量 100 万
     }
+    // 新增公开的 mint 方法，只有合约所有者可以调用
+    function mint(address to, uint256 amount) external {
+        _mint(to, amount);
+    }
 }
